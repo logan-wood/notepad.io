2996576917bf16d6e33796aacf2a86e8e5fad8c7
Hgarnell <hannah@whiterabbit.com>
2023-04-04 09:46:20 +1200

    adding to more css and editing mainpage

--- client/src/pages/mainpage.js
+++ client/src/pages/mainpage.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import './assets/mainpage.css';
 
-function App() {
+function Mainpage() {
   return (
     <div className="App">
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Fjalla+One&family=Nunito:wght@300&display=swap');
+      </style>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
     </div>
   );
 }
 
-export default App;
+export default Mainpage;
