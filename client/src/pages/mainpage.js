47b31b76c899356d5cfbb39e88135de2908fe2f6
Hgarnell <hannah@whiterabbit.com>
2023-04-05 15:51:30 +1200

    Fixed display and added header and class menu ish

--- client/src/pages/mainpage.js
+++ client/src/pages/mainpage.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
-import './assets/mainpage.css';
+import '../assets/mainpage.css';
 
 function Mainpage() {
   return (
-    <div className="App">
+    <div className="mainpage">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <style>
         @import url('https://fonts.googleapis.com/css2?family=Fjalla+One&family=Nunito:wght@300&display=swap');
       </style>
-      <header className="App-header">
+      <header className="mainpage_header">
+        NotePad.io
       </header>
+    <div className="classmenu">
+      testing
+    </div>
 
     </div>
   );
