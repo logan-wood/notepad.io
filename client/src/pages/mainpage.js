3e91b38c6b01fd9285734144c9f78675cd121175
Hgarnell <hannah@whiterabbit.com>
2023-04-11 11:33:12 +1200

    Added class stylings and most of animated functionaility of the sidenav

--- client/src/pages/mainpage.js
+++ client/src/pages/mainpage.js
@@ -4,7 +4,29 @@ import React, {useState} from 'react';
 import SideNav from '../shared/SideNav'
 
 function Mainpage() {
-  const [isNavOpen, setIsNavOpen] = useState(false);
+  //**test class data */
+  const [classes, setClasses] = useState([
+    {
+      id: 1,
+      name: 'Class 1',
+      subnotes: [
+        { id: 1, title: 'Subnote 1' },
+        { id: 2, title: 'Subnote 2' },
+        // Add more subnotes as needed
+      ],
+    },
+    {
+      id: 2,
+      name: 'Class 2',
+      subnotes: [
+        { id: 1, title: 'Subnote 1' },
+        { id: 2, title: 'Subnote 2' },
+        // Add more subnotes as needed
+      ],
+    }
+  ])
+
+  const [isNavOpen, setIsNavOpen] = useState(true);
 
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
@@ -17,7 +39,7 @@ function Mainpage() {
         @import url('https://fonts.googleapis.com/css2?family=Fjalla+One&family=Nunito:wght@300&display=swap');
       </style>
       <Header />
-    <SideNav isOpen={isNavOpen} toggleNav = {toggleNav} className="classmenu"/>
+    <SideNav isOpen={isNavOpen} toggleNav = {toggleNav} classes={classes} className="classmenu"/>
     <div className="note">
       testing
     </div>
