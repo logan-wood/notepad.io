551f490fb4ca344dbfc8df9da71316de56c7da2c
Theo Benvie-Watson <theo@theobwatson.com>
2023-04-12 16:42:22 +1200

    added email sign in

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBI6Z_GHuX9nxZSEqMn3K5kixdiwSGtzvo",
@@ -12,5 +13,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore();
 
-export { app, auth };
+export { auth, db };
--- client/src/loginpage/Login.js
+++ client/src/loginpage/Login.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../shared/Header.js";
-import { auth } from "../firebase";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { Button } from "react-bootstrap";
 import "./Login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const auth = getAuth();
   const provider = new GoogleAuthProvider();
 
   const signInWithGoogle = () => {
@@ -18,15 +25,46 @@ const Login = () => {
       });
   };
 
+  const signInWithEmail = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <>
       <Header showButton={false} />
       <div className="login-page">
         <div className="login-box">
           <h2 className="login-title">Log in</h2>
-          <Button variant="primary" className="google-signin-button" onClick={signInWithGoogle}>
+          <input
+            className="input-field"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="input-field"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button variant="primary" className="email-signin-button" onClick={signInWithEmail}>
+            Sign in with Email
+          </Button>
+          <div className="or-divider">
+            <span className="or-text">or</span>
+          </div>
+          <Button variant="primary"  className="google-signin-button" onClick={signInWithGoogle}>
             Sign in with Google
           </Button>
+          <div className="register-text">Don't have an account?</div>
         </div>
       </div>
     </>
