d82c12a4a541d5e4e2016e01ed3123fc6ef3e1d7
Theo Benvie-Watson <theo@theobwatson.com>
2023-04-12 18:43:04 +1200

    added transition between pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,32 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Home from "./homepage/Home";
 import Login from "./loginpage/Login";
+import "./AppTransitions.css";
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <TransitionGroup>
+      <CSSTransition key={location.pathname} classNames="fade" timeout={1000}>
+        <div className="route-container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+}
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<><Home /></>} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+        <AnimatedRoutes />
       </div>
     </Router>
   );
