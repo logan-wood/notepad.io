d441da0ef361f63010da3a6480cd63277b1bd3fc
Timothy Young <bht4069@autuni.ac.nz>
2023-04-16 14:22:16 +1200

    added /updateUser endpoint; my autoformatter auto-formatted a bunch of stuff also.

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,19 +1,32 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // import controllers
-const userController = require('../controllers/userController')
+const userController = require("../controllers/userController");
 
-router.get('/', function(req, res) {
-    res.json(JSON.parse('{"message": "This is a response from the server!"}'))
-})
+router.get("/", function (req, res) {
+  res.json(JSON.parse('{"message": "This is a response from the server!"}'));
+});
 
-router.post('/addNewUser', (req, res) => {
-    userController.addNewUser(req.body.displayName, req.body.email, req.body.password)
-})
+router.post("/addNewUser", (req, res) => {
+  userController.addNewUser(
+    req.body.displayName,
+    req.body.email,
+    req.body.password
+  );
+});
 
-router.get('/getUser', function(req, res) {
-    userController.getUser(req, res)
-})
+router.get("/getUser", function (req, res) {
+  userController.getUser(req, res);
+});
 
-module.exports = router
+router.put("/updateUser", (req, res) => {
+  const classes = req.body.classes;
+  if (classes) {
+    userController.updateUser(req, res, classes);
+  } else {
+    res.status(404).send("Classes not found.");
+  }
+});
+
+module.exports = router;
