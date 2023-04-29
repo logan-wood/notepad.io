7d58433dc0cea2fc58907766f40f79bda1c43b6d
Timothy Young <bht4069@autuni.ac.nz>
2023-04-29 12:29:59 +1200

    added comments about the endpoints.

--- server/routes/router.js
+++ server/routes/router.js
@@ -7,19 +7,22 @@ const userController = require("../controllers/userController");
 router.get("/", function (req, res) {
   res.json(JSON.parse('{"message": "This is a response from the server!"}'));
 });
-
+// adds a new user to the database
+// query params: uid
 router.post("/user/add", (req, res) => {
   userController.addNewUser(req, res);
 });
-
+// retrieves all user data based on uid
+// query params: uid
 router.get("/user/get", function (req, res) {
   userController.getUser(req, res);
 });
-
+// updates and overwrites single class, or adds them if they don't already exist.
 router.put("/user/:id/updateClassNote", (req, res) => {
   const id = req.params.id;
   userController.updateClassNote(req, res, id);
 });
+// returns all classes as JSONs.
 router.get("/user/:id/getAllClassNotes", (req, res) => {
   const id = req.params.id;
   userController.getAllClassNotes(req, res, id);
