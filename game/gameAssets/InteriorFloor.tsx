28be0b608367e9a3d8713feade6b73a1e31c6290
Matei Crisea <nrs3007@autuni.ac.nz>
2023-04-30 13:54:56 +1200

    added game directory

---
+++ game/gameAssets/InteriorFloor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="InteriorFloor" tilewidth="16" tileheight="16" tilecount="374" columns="22">
+ <image source="../../../../../../Downloads/NinjaAdventure/NinjaAdventure/NinjaAdventure/Backgrounds/Tilesets/Interior/TilesetInteriorFloor.png" width="352" height="272"/>
+</tileset>
---
+++ game/gameAssets/frogCharacter.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="character" tilewidth="16" tileheight="16" tilecount="28" columns="4">
+ <image source="../../../../../../Downloads/NinjaAdventure/NinjaAdventure/NinjaAdventure/Actor/Characters/MaskFrog/SpriteSheet.png" width="64" height="112"/>
+</tileset>
