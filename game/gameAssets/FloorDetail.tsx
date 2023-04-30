7207fff60fa1d3b72fcfba86c00f8e53462367b0
Matei Crisea <nrs3007@autuni.ac.nz>
2023-04-30 16:15:38 +1200

    cleanup

--- game/gameAssets/FloorDetail.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="FloorDetail" tilewidth="16" tileheight="16" tilecount="80" columns="16">
- <image source="../../../../../../Downloads/NinjaAdventure/NinjaAdventure/NinjaAdventure/Backgrounds/Tilesets/TilesetFloorDetail.png" width="256" height="80"/>
-</tileset>
--- game/gameAssets/OutdoorFloor.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="OutdoorFloor" tilewidth="16" tileheight="16" tilecount="572" columns="22">
- <image source="../../../../../../Downloads/NinjaAdventure/NinjaAdventure/NinjaAdventure/Backgrounds/Tilesets/TilesetFloor.png" width="352" height="417"/>
-</tileset>
