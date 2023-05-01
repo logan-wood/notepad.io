74a421594e42074610045ae4c8b22dc4df2a92ba
Theo Benvie-Watson <theo@theobwatson.com>
2023-05-02 11:30:37 +1200

    fix header formatting

--- client/src/shared/Header.js
+++ client/src/shared/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import "./Header.css";
 import logo from "./notey.png";
 import { Link } from "react-router-dom";
@@ -7,27 +7,19 @@ import { Link } from "react-router-dom";
 const Header = ({ showButton }) => {
   return (
     <header className="header">
-      <Container fluid>
-        <Row className="align-items-center">
-          <Col xs={6} sm={6} md={6} lg={6} className="d-flex">
-            <div className="logo-title d-flex align-items-center">
-              <Link to="/">
-                <img src={logo} alt="Logo" className="logo" />
-              </Link>
-              <h1 className="app-title">Notepad.io</h1>
-            </div>
-          </Col>
-          <Col xs={6} sm={6} md={6} lg={6} className="text-right">
-            {showButton && (
-              <Link to="/login">
-                <Button variant="primary" className="signup-login-button">
-                  Log in / Sign up
-                </Button>
-              </Link>
-            )}
-          </Col>
-        </Row>
-      </Container>
+      <div className="logo-title d-flex align-items-center">
+        <Link to="/">
+          <img src={logo} alt="Logo" className="logo" />
+        </Link>
+        <h1 className="app-title">Notepad.io</h1>
+      </div>
+      {showButton && (
+        <Link to="/login">
+          <Button variant="primary" className="signup-login-button">
+            Log in / Sign up
+          </Button>
+        </Link>
+      )}
     </header>
   );
 };
