e694dbeaf0c4f3706caf68dde76cf5b2b3650186
Hgarnell <hannah@whiterabbit.com>
2023-05-15 05:41:45 +1200

    got rid of the popup alert, and usedd stack overflow to help
    https://stackoverflow.com/questions/72495838/copy-text-to-clipboard-using-reactjs

--- client/src/mainpage/ShareModal.js
+++ client/src/mainpage/ShareModal.js
@@ -1,25 +1,44 @@
 import React, { useState, useEffect } from "react";
-import CopyToClipboard from "copy-to-clipboard";
 import "./ShareModal.css";
 
 const ShareModal = ({ onClose, isOpen }) => {
-    const [copyText, setCopyText] = useState("https://notepad.io/sharelink/sdkfjhsdkjf");
+  const [copyText, setCopyText] = useState(
+    "https://notepad.io/sharelink/sdkfjhsdkjf"
+  );
+  const handleWrapperClick = (event) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
 
-    const handleCopy = () => {
-      console.log("Copied");
-    };
   return (
-    <div className={`modalShareWrapper ${isOpen ? "open" : ""}`}>
+    <div
+      className={`modalShareWrapper ${isOpen ? "open" : ""}`}
+      onClick={handleWrapperClick}
+    >
       <div className="shareModal">
         <h1> Share this note!</h1>
         <hr />
         <input type="text" value={copyText} readOnly />
 
         <div className="modalContent">
-            <button className="share" onClick={handleCopy}>
-              Copy
-            </button>
-          <CopyToClipboard text="https://notepad.io/sharelink/sdkfjhsdkjf"/>
+          <button
+            className="share"
+            //https://stackoverflow.com/questions/72495838/copy-text-to-clipboard-using-reactjs
+            onClick={async () => {
+              if ("clipboard" in navigator) {
+                await navigator.clipboard.writeText(copyText);
+              } else {
+                document.execCommand(
+                  "copy",
+                  true,
+                  copyText
+                );
+              }
+            }}
+          >
+            Copy
+          </button>
         </div>
       </div>
     </div>
