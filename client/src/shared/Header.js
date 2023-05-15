8a0103488662a29dcbe048948f6dc213a6f01498
Theo Benvie-Watson <theo@theobwatson.com>
2023-05-16 10:22:45 +1200

    fixing syntax

--- client/src/shared/Header.js
+++ client/src/shared/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import "./Header.css";
 import logo from "./notey.png";
@@ -31,15 +31,12 @@ const Header = ({
     dispatch({ type: 'CLEAR_USER' });
   };
 
-const Header = ({ showButtons, pageName, showSignOutButton }) => {
   return (
     <header className="header">
       <div className="logo-title d-flex align-items-center">
-        {/** Link to a specified page when Logo is clicked */}
         <Link to={pageName}>
           <img src={logo} alt="Logo" className="logo" />
         </Link>
-        {/** Link to a specified page when Title is clicked */}
         <Link to={pageName} className="app-title link-class">
           <h1 className="app-title">Notepad.io</h1>
         </Link>
@@ -58,31 +55,19 @@ const Header = ({ showButtons, pageName, showSignOutButton }) => {
           </Link>
         </div>
       )}
-      {/* Conditionally render the dark mode, profile, settings, and sign out buttons */}
       {showDashBoardButtons && (
         <div className="dashboard-buttons-container">
-          {/* switches the text of dark mode button depending on the isDarkMode boolean */}
           <Button className="dark-mode-toggle" onClick={handleDarkMode}>
             {isDarkMode ? "Light Mode" : "Dark Mode"}
           </Button>
-          {/* links to setting page when clicked */}
           <Link to="/settings" className="settings-button">
             <img src={settingsIcon} alt="Settings" className="settings-icon" />
           </Link>
-          {/* links to profile page when clicked */}
           <Link to="/profile" className="profile-button">
             <img src={profileIcon} alt="Profile" className="profile-icon" />
           </Link>
-          {/* links to landing page when clicked */}
           <Link to="/" className="sign-out-button" onClick={logoutUser}>
             <img src={signOutIcon} alt="Sign Out" className="sign-out-icon" />
-      {showSignOutButton && (
-        <div className="sign-out-button">
-          <Link to="/">
-            <Button variant="primary" className="sign-out-button">
-              <img src={signOutIcon} alt="Sign Out" className="sign-out-icon" />
-              Sign Out
-            </Button>
           </Link>
         </div>
       )}
