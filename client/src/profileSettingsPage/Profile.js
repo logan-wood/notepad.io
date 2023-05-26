dce9bc6f359cc6fe11843853fd073c36722ad04b
Hgarnell <hannah@whiterabbit.com>
2023-05-27 09:52:46 +1200

    added cancel and verification feature
    Goes back to original username and email if canclled is pressed, and user needs to enter correct email for it to change properly

--- client/src/profileSettingsPage/Profile.js
+++ client/src/profileSettingsPage/Profile.js
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Components/Profile.css";
 import Header from "../shared/Header";
 import profilePic from "./Components/notelo-profile.png";
-const Profile = ({user}) => {
+const Profile = ({ user }) => {
   //constants
   // state variables for edit mode and edit values
-  const [editMode, setEditMode] = React.useState(false);
-  const [editEmail, setEditEmail] = React.useState(user.email);
-  const [editUsername, setEditUsername] = React.useState(user.username);
+  const [editMode, setEditMode] = useState(false);
+  const [editEmail, setEditEmail] = useState(user.email);
+  const [editUsername, setEditUsername] = useState(user.username);
+
+  //state variables for the correct username and emaikl
+  const [username, setUsername] = useState(user.username);
+  const [email, setEmail] = useState(user.email);
 
   //save the edits
   const handleSave = () => {
     //check if email is valid
     if (!validEmail(editEmail)) {
       alert("Please enter a valid email");
+      setEditEmail(email);
       setEditMode(false);
 
-      return;
+    } else {
+      //close editmode and set new email
+      setEmail(editEmail);
+      setEditMode(false);
     }
-    //close editmode
-    setEditMode(false);
   };
-
+ const handleCancel = () => {
+  
+     //close editmode and set new email
+      setEditEmail(email);
+      setEditUsername(username);
+     setEditMode(false);
+   
+ };
   //function to check regex of email
   const validEmail = (email) => {
     //regex for valid email
@@ -33,61 +46,58 @@ const Profile = ({user}) => {
   //render
   return (
     <>
-     
-          <img src={profilePic} alt="Logo" className="profile-pic" />
+      <img src={profilePic} alt="Logo" className="profile-pic" />
 
-          <div className="profile-info">
-            <span className="userInfoTitle">Profile Information</span>
-            <hr></hr>
-            {!editMode ? (
-              <>
-                {user && (
-                  <>
-                    <span className="userInfoItems">
-                      <span className="userInfoTitle">UID:</span> {user.uid}
-                    </span>
-                    <span className="userInfoItems">
-                      <span className="userInfoTitle">Email:</span> {editEmail}
-                    </span>
-                    <span className="userInfoItems">
-                      <span className="userInfoTitle">Username:</span>{" "}
-                      {editUsername}
-                    </span>
-                  </>
-                )}
-                <span className="profile-buttons">
-                  <button onClick={() => setEditMode(true)}>Edit</button>
-                </span>
-              </>
-            ) : (
+      <div className="profile-info">
+        <span className="userInfoTitle">Profile Information</span>
+        <hr></hr>
+        {!editMode ? (
+          <>
+            {user && (
               <>
                 <span className="userInfoItems">
                   <span className="userInfoTitle">UID:</span> {user.uid}
                 </span>
                 <span className="userInfoItems">
-                  <span className="userInfoTitle">Email:</span>
-                  <input
-                    type="text"
-                    value={editEmail}
-                    onChange={(e) => setEditEmail(e.target.value)}
-                  />
+                  <span className="userInfoTitle">Email:</span> {email}
                 </span>
                 <span className="userInfoItems">
-                  <span className="userInfoTitle">Username:</span>
-                  <input
-                    type="text"
-                    value={editUsername}
-                    onChange={(e) => setEditUsername(e.target.value)}
-                  />
-                </span>
-                <span className="profile-buttons">
-                  <button onClick={handleSave}>Save</button>
-                  <button onClick={() => setEditMode(false)}>Cancel</button>
+                  <span className="userInfoTitle">Username:</span> {username}
                 </span>
               </>
             )}
-          </div>
-        
+            <span className="profile-buttons">
+              <button onClick={() => setEditMode(true)}>Edit</button>
+            </span>
+          </>
+        ) : (
+          <>
+            <span className="userInfoItems">
+              <span className="userInfoTitle">UID:</span> {user.uid}
+            </span>
+            <span className="userInfoItems">
+              <span className="userInfoTitle">Email:</span>
+              <input
+                type="text"
+                value={editEmail}
+                onChange={(e) => setEditEmail(e.target.value)}
+              />
+            </span>
+            <span className="userInfoItems">
+              <span className="userInfoTitle">Username:</span>
+              <input
+                type="text"
+                value={editUsername}
+                onChange={(e) => setEditUsername(e.target.value)}
+              />
+            </span>
+            <span className="profile-buttons">
+              <button onClick={handleSave}>Save</button>
+              <button onClick={handleCancel}>Cancel</button>
+            </span>
+          </>
+        )}
+      </div>
     </>
   );
 };
