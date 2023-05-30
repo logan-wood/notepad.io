5956a1f42531006b974eacd3ba1cca6aa794db28
logan-wood <logan.loganwood@gmail.com>
2023-05-30 15:46:25 +1200

    conditionally redirect user

--- client/src/homepage/Home.js
+++ client/src/homepage/Home.js
@@ -4,19 +4,33 @@ import divider from "./divider_strip.png";
 import { Button } from "react-bootstrap";
 import Header from "../shared/Header";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const MainContent = () => {
+  //user object
+  const user = useSelector((state) => state.user);
+
   return (
     <>
       <Header showButtons={true} pageName = "/"/>
       <div className="main-content">
         <div className="text-container">
           <h1 className="title">Notepad.io</h1>
+          { user != null ? 
+          <Link to="/dashboard">
+            <Button variant="primary" className="get-notey-button">
+              Get notey!
+            </Button>
+          </Link>
+
+          :
+
           <Link to="/signup">
             <Button variant="primary" className="get-notey-button">
               Get notey!
             </Button>
           </Link>
+          }
         </div>
         <img className="divider-image" src={divider} alt="Divider" />
       </div>
